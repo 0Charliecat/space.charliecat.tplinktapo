1e3c96d10d669ebea47ae14f3982328205c4592a
Slovak_Cat <slovak_cat@boun.cr>
2021-07-21 17:10:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const tapo = require("tp-link-tapo-connect")
+const key = require("./.password.json")
 
-tapo.loginDeviceByIp("k890164@gmail.com", "nocxu7-vycvyk-rehsuJ", "192.168.88.28").then((value) => {
+tapo.loginDeviceByIp(key.email, key.password, "192.168.88.28").then((value) => {
      tapo.getDeviceInfo(value).then(console.log)
     })
