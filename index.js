b90a3b35b2042240e0d47c23ff51593d717e7e98
Slovak_Cat <slovak_cat@boun.cr>
2021-07-21 17:05:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,5 @@
-const fetch = require('node-fetch')
-var url = "https://eu-wap.tplinkcloud.com"
+const tapo = require("tp-link-tapo-connect")
 
-async function tokenner(email, password) {
-    var payload = {
-            "method": "login",
-            "params": {
-                "appType": "Tapo_Ios",
-                "cloudUserName": email,
-                "cloudPassword": password,
-                "terminalUUID": "0A950402-7224-46EB-A450-7362CDB902A2"
-            }
-        }
-
-    var data = await fetch(url, { method: 'POST', body: payload });
-    var json = await data.json();
-    return json.result.token;
-}
+tapo.loginDeviceByIp("k890164@gmail.com", "nocxu7-vycvyk-rehsuJ", "192.168.88.28").then((value) => {
+     tapo.getDeviceInfo(value).then(console.log)
+    })
